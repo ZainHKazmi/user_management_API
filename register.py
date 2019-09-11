901f3c4276030a1e51c92680ef4dc3f7c54167be
Zain Kazmi <zkazmi786@hotmail.ca>
2019-09-11 19:28:52 -0400

    create registration api outline

---
+++ register.py
@@ -0,0 +1,16 @@
+class register:
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+    def password_verification(self):
+        ##TODO: implement password_verification
+        pass
+
+    def check_username(self):
+        ##TODO: Implement username check
+        pass
+
+    if __name__ == "__main__":
+        pass
